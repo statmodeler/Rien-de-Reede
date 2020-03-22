0c9a32470f110f06431c6192dd6d89298c4c529e
statmodeler <holtman@pme.me>
2020-03-22 20:07:44 +0100

    kleine aanpassingen

--- wordpress/wp-content/themes/riendereede/functions.php
+++ wordpress/wp-content/themes/riendereede/functions.php
@@ -1,3 +1,15 @@
 <?php
 	add_theme_support( 'title-tag' );
+
+	add_action( 'wp_enqueue_scripts', 'custom_load_contact_form_resources', 1 );
+	function custom_load_contact_form_resources() {
+		global $post;
+
+		if ( isset( $post ) && is_singular() && has_shortcode( $post->post_content, 'contact-form-7' ) ) {
+			return;
+		}
+
+		remove_action( 'wp_enqueue_scripts', 'wpcf7_do_enqueue_scripts' );
+		remove_action( 'wp_enqueue_scripts', 'wpcf7_recaptcha_enqueue_scripts' );
+	}
 ?>
